e60918cdfaeed7f6ff24ac962048113c3f6e6868
UnaiHerna <uhernandez@cimico.tech>
2025-02-18 15:23:01 +0100

    pipes

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidateUserPipe } from './pipes/validate-user/validate-user.pipe';
 
 @Controller({})
 export class HelloController {
@@ -9,5 +10,26 @@ export class HelloController {
 		response.status(200).json({
 			message: 'Hello World!',
 		});
+	}
+
+	@Get('notfound')
+	@HttpCode(404)
+	notFoundPage(){
+		return "Not Found Page";
+	}
+
+	@Get('/ticket/:num')
+	getNumber(@Param('num', ParseIntPipe) num: number){
+		return num + 14;	
+	}
+
+	@Get('/user/:status')
+	isUserActive(@Param('status') status: boolean){
+		return status;
+	}
+
+	@Get('greet')
+	greet(@Query(ValidateUserPipe) query: {name: string, age: number}){
+		return `Hello ${query.name}, you are ${query.age} years old`
 	}
 }
